a6870a73ad00d90ad5cecf0983bd659fe9135eb5
Koichi Tanaka <tanaka51.jp@gmail.com>
2020-05-11 01:12:55 +0900

    Render 30x30 cells

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,10 +2,34 @@ import '../sass/main.scss'
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
+const WIDTH = 30;
+const HEIGHT = 30;
+
+enum Cell {
+  Live,
+  Die
+}
+
 const Hello: React.FC<{name: string}> = (props) => {
+  // 2-dimensional array filled with Cell.Die
+  const board: Cell[][] = new Array(HEIGHT).fill(new Array(WIDTH).fill(Cell.Die));
+
   return (
     <div>
-      <p>{props.name}</p>
+      <h1>{props.name}</h1>
+      {board.map((row, y) => {
+        return(
+          <div className={`row row--${y}`} key={y}>
+            {row.map((cell, x) => {
+              const cellNumber = y * WIDTH + x;
+
+              return (
+                <span className={`cell cell--${cellNumber}`} key={cellNumber}>{cell}</span>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }

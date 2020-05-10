71bf6f12a821621c0d73d89d2aace7df89bb7bb8
Koichi Tanaka <tanaka51.jp@gmail.com>
2020-05-10 22:29:30 +0900

    Setup for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
   devServer: {
     contentBase: 'dist',
     open: true

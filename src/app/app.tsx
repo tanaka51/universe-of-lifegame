402fbb9d4a5d929b1d8fcc322eb4ee975ef928ed
Koichi Tanaka <tanaka51.jp@gmail.com>
2020-05-10 23:48:45 +0900

    First React samaple code

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,14 @@
-import '../sass/main.scss';
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
 
-console.log('test');
+const Hello: React.FC<{name: string}> = (props) => {
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+  )
+}
+
+ReactDom.render(
+  <Hello name='lifegame' />, document.getElementById('root')
+)

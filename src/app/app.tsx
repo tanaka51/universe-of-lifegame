88cd69aadda53870aa0b4cffb2fcb77d3eda0aaf
Koichi Tanaka <tanaka51.jp@gmail.com>
2020-05-13 04:52:37 +0900

    $ npx eslint --fix src -c .eslintrc.json --ext ts,tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,9 @@
-import '../sass/main.scss';
-import React, { useState, useEffect } from 'react';
-import ReactDom from 'react-dom';
+import '../sass/main.scss'
+import React, { useState, useEffect } from 'react'
+import ReactDom from 'react-dom'
 
-const WIDTH = 100;
-const HEIGHT = 100;
+const WIDTH = 100
+const HEIGHT = 100
 
 enum CellStatus {
   Live = 'live', Die = 'die'
@@ -20,106 +20,105 @@ interface Row {
 }
 
 const initialBoard = (): Row[] => {
-  const board: Row[] = [];
+  const board: Row[] = []
 
   for (let y = 0; y < HEIGHT; y += 1) {
-    board[y] = { id: y, cells: [] };
+    board[y] = { id: y, cells: [] }
 
     for (let x = 0; x < WIDTH; x += 1) {
-      let status: CellStatus = CellStatus.Die;
+      let status: CellStatus = CellStatus.Die
 
-      const xx = WIDTH / 2;
-      const yy = HEIGHT / 2;
+      const xx = WIDTH / 2
+      const yy = HEIGHT / 2
       if (x === xx && y === yy) {
-        status = CellStatus.Live;
+        status = CellStatus.Live
       } else if (x === (xx - 1) && y === yy) {
-        status = CellStatus.Live;
+        status = CellStatus.Live
       } else if (x === xx && y === (yy + 1)) {
-        status = CellStatus.Live;
+        status = CellStatus.Live
       } else if (x === xx && y === (yy + 2)) {
-        status = CellStatus.Live;
+        status = CellStatus.Live
       } else if (x === (xx + 1) && y === (yy + 1)) {
-        status = CellStatus.Live;
+        status = CellStatus.Live
       }
 
       board[y].cells[x] = {
         id: y * WIDTH + x,
         status,
-      };
+      }
     }
   }
 
-  return board;
-};
+  return board
+}
 
 const neighborhoods = (x: number, y: number): [number, number][] => {
-  const pos: [number, number][] = [];
+  const pos: [number, number][] = []
 
   if ((y - 1) > 0) {
-    pos.push([x, y - 1]);
-    if ((x - 1) > 0) pos.push([x - 1, y - 1]);
-    if ((x + 1) < WIDTH) pos.push([x + 1, y - 1]);
+    pos.push([x, y - 1])
+    if ((x - 1) > 0) pos.push([x - 1, y - 1])
+    if ((x + 1) < WIDTH) pos.push([x + 1, y - 1])
   }
   if ((y + 1) < HEIGHT) {
-    pos.push([x, y + 1]);
-    if ((x - 1) > 0) pos.push([x - 1, y + 1]);
-    if ((x + 1) < WIDTH) pos.push([x + 1, y + 1]);
+    pos.push([x, y + 1])
+    if ((x - 1) > 0) pos.push([x - 1, y + 1])
+    if ((x + 1) < WIDTH) pos.push([x + 1, y + 1])
   }
-  if ((x - 1) > 0) pos.push([x - 1, y]);
-  if ((x + 1) < WIDTH) pos.push([x + 1, y]);
+  if ((x - 1) > 0) pos.push([x - 1, y])
+  if ((x + 1) < WIDTH) pos.push([x + 1, y])
 
-  return pos;
-};
+  return pos
+}
 
 const calcDeadOrAlive = (board: Row[], x: number, y: number): CellStatus => {
   const liveCount = neighborhoods(x, y).map(
     (pos: [number, number]) => board[pos[1]].cells[pos[0]].status,
   ).filter(
     (s) => s === CellStatus.Live,
-  ).length;
+  ).length
 
   if (liveCount === 3) {
-    return CellStatus.Live;
+    return CellStatus.Live
   }
   if (liveCount === 2 && board[y].cells[x].status === CellStatus.Live) {
-    return CellStatus.Live;
+    return CellStatus.Live
   }
-  return CellStatus.Die;
-};
+  return CellStatus.Die
+}
 
 const nextBoard = (currentBoard: Row[]): Row[] => {
-  const next: Row[] = [];
+  const next: Row[] = []
 
   for (let y = 0; y < HEIGHT; y += 1) {
-    next[y] = { id: y, cells: [] };
+    next[y] = { id: y, cells: [] }
 
     for (let x = 0; x < WIDTH; x += 1) {
       next[y].cells[x] = {
         id: y * WIDTH + x,
         status: calcDeadOrAlive(currentBoard, x, y),
-      };
+      }
     }
   }
 
-  return next;
-};
+  return next
+}
 
 const Board: React.FC = () => {
-  const [board, setBoard] = useState(initialBoard);
+  const [board, setBoard] = useState(initialBoard)
 
-  let timerId: ReturnType<typeof setTimeout> | null = null;
+  let timerId: ReturnType<typeof setTimeout> | null = null
   useEffect(() => {
     timerId = setTimeout(() => {
-      setBoard(nextBoard(board));
-    }, 1);
+      setBoard(nextBoard(board))
+    }, 1)
 
     return (): void => {
       if (timerId) {
-        clearTimeout(timerId);
+        clearTimeout(timerId)
       }
-    };
-  });
-
+    }
+  })
   return (
     <div className="board-inner">
       {board.map((row, y) => (
@@ -128,9 +127,9 @@ const Board: React.FC = () => {
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
 ReactDom.render(
   <Board />, document.getElementById('board'),
-);
+)
